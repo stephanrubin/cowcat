bd655ecc89124a18dbc2654ffc4b384420149eca
Stephan Rubin <stephan.rubin@gmail.com>
2016-02-14 15:08:24 -0500

    added user model scaffolding

---
+++ db/migrate/20160214195045_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.text :username
+      t.text :fname
+      t.text :lname
+      t.text :email
+
+      t.timestamps null: false
+    end
+  end
+end
