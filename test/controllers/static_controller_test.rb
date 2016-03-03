5526629f95b59c6d1903d53b3fda2a0333bbc876
Stephan Rubin <stephan.rubin@gmail.com>
2016-03-03 15:12:58 -0500

    added title templating and refactored application template

--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -1,23 +1,28 @@
 require 'test_helper'
 
 class StaticControllerTest < ActionController::TestCase
+
   test "should get help" do
     get :help
+    assert_select "title", "Help | CowCat"
     assert_response :success
   end
 
   test "should get home" do
     get :home
+    assert_select "title", "Welcome to CowCat"
     assert_response :success
   end
 
   test "should get notfound" do
     get :notfound
+    assert_select "title", "Not Found | CowCat"
     assert_response :success
   end
 
   test "should get about" do 
     get :about
+    assert_select "title", "About | CowCat"
     assert_response :success
   end
 
